ca87ea3e3e93196ad0fc63c4e4d745783e0a4620
Aidan Johansson <80654114+aidanj0@users.noreply.github.com>
2022-02-10 21:59:55 -0500

    updated

--- main.js
+++ main.js
@@ -2,13 +2,19 @@
 
 In-class activity 07 starter code
 Prof. Mosca 
-Modified: 12/06/21 
+Modified: 12/06/21
+
+REFERENCED: MDN Web Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 */
 
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
-    // You will need to modiffy the body of this function as described in the assignment Readme
+    // You will need to modify the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let div = document.getElementById("random_num");
+    min = Math.ceil(1);
+    max = Math.floor(11);
+    div.append(Math.floor((Math.random() * (10) + 1)).toString() + " ");
+
 }
